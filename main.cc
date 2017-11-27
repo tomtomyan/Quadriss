2fe6727b306705a308961278e95c9eff74ae56ee
Tom Yan <tom.yan@uwaterloo.ca>
2017-11-27 15:05:42 -0500

    added command interpreter

---
+++ main.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+#include "grid.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  cin.exceptions(ios::eofbit|ios::failbit);
+  string cmd;
+
+  try {
+    while (true) {
+      cin >> cmd;
+      if (cmd == "left") {
+
+      } else if (cmd == "right") {
+
+      } else if (cmd == "down") {
+
+      } else if (cmd == "clockwise") {
+
+      } else if (cmd == "counterclockwise") {
+
+      } else if (cmd == "drop") {
+
+      } else if (cmd == "levelup") {
+
+      } else if (cmd == "leveldown") {
+
+      } else if (cmd == "norandom") {
+
+      } else if (cmd == "random") {
+
+      } else if (cmd == "sequence") {
+
+      } else if (cmd == "restart") {
+
+      } else if (cmd == "hint") {
+
+      } 
+
+
+    }
+  } catch (ios::failure &) {}
+
+}
